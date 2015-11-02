449283af5466c78f1b9b257fe00f38e58c5c2c3c
RStoeckl <sto16373@spengergasse.at>
2015-11-02 13:55:01 +0100

    added bugfree navigation drawer

---
+++ app/src/main/java/at/mvl/musikvereinleopoldsdorf/PreisFragment.java
@@ -0,0 +1,27 @@
+package at.mvl.musikvereinleopoldsdorf;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by richi on 02.11.15.
+ */
+public class PreisFragment extends Fragment {
+
+    private View contentView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        contentView = inflater.inflate(R.layout.preis_layout, null);
+        return contentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
